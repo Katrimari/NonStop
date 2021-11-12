ca2b29b364f434d81e62c6171120a03893e7b71a
katrimari <katri.lepola@edu.keuda.fi>
2021-11-12 14:33:46 +0200

    scriptiä

---
+++ omia kokeiluja/script.js
@@ -0,0 +1,17 @@
+function calcCircleArea(a, b)
+{
+var area = a * (2 * b);
+  return Math.round(area);
+}
+var area = calcCircleArea(Math.PI, 2,1);
+
+
+console.log(area)
+
+function aakkosJar()
+{
+  var sana1 = prompt("anna eka sana");
+  var sana2 = prompt("anna toka sana");
+  return [sana1, sana2].sort()[0]
+}
+console.log(aakkosJar());
