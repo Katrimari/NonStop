53bf4954d3c48418a34e5c31477290f1ae806345
katrimari <katri.lepola@edu.keuda.fi>
2021-12-07 12:00:31 +0200

    jotain

---
+++ eka Javascript/viikko2/koodaushaaste/script.js
@@ -0,0 +1,107 @@
+const arr = [8, 4, 9, 6, 2]
+function pieninSuurin()
+{
+  arr.sort();
+  var pienin = arr.shift();
+  var suurin = arr.pop();
+  console.log("pienin: " + pienin +" suurin: " + suurin);
+}
+
+ var nro = [6]
+ function rillinenRiton(a, b)
+ {
+   if(nro % 2==0)
+   {
+     console.log ("parillinen");
+   }
+   else{
+     console.log("pariton");
+   }
+}
+
+
+
+ function kuuKaudet()
+ {
+ var kk = document.getElementById("kuukausi").value;
+
+    switch (kk){
+      case "1":
+      console.log("Tammikuu");
+      break;
+      case "2":
+      console.log("Helmikuu");
+      break;
+      case "3":
+      console.log("Maaliskuu");
+      break;
+      case "4":
+      console.log("Huhtikuu");
+      break;
+      case "5":
+      console.log("Toukokuu");
+      break;
+      case "6":
+      console.log("Kesäkuu");
+      break;
+      case "7":
+      console.log("Heinäkuu");
+      break;
+      case "8":
+      console.log("Elokuu");
+      break;
+      case "9":
+      console.log("Syyskuu");
+      break;
+      case "10":
+      console.log("Lokakuu");
+      break;
+      case "11":
+      console.log("Marraskuu");
+      break;
+      case "12":
+      console.log("Joulukuu");
+      break;
+      default:
+      console.log("Et valinnut arvoa välillä 1-12");
+    }
+
+}
+
+function henkilo(etunimi, sukunimi, losoite, pnro, ptp, puh, email)
+{
+  this.etunimi = etunimi,
+  this.sukunimi = sukunimi,
+  this.lahiosoite = losoite,
+  this.postinumero = pnro,
+  this.postitoimipaikka = ptp,
+  this.puhelinnumero = puh,
+  this.sahkoposti = email
+
+  this.tiedot = NaytaTiedot;
+}
+
+function NaytaTiedot()
+{
+  var etunimi = this.etunimi;
+  var sukunimi = this.sukunimi;
+  var losoite = this.lahiosoite;
+  var pnro = this.postinumero;
+  var ptp = this.postitoimipaikka;
+  var puh = this.puhelinnumero;
+  var email = this.sahkoposti;
+
+}
+
+var Hannu = new henkilo("Hannu", "Markkanen", "kervontie 2", "03123", "Kerala", "0506253794", "hannu.markkanen@kerala.fi");
+var Katri = new henkilo("Katri", "Lepola", "Kauppatie 2", "04310", "Tuusula", "0445000853", "katri.lepola@edu.keuda.fi");
+var Mira = new henkilo("Mira", "Kujala", "orakuja 6A", "04500", "Kellokoski", "0464892614", "mira.kujala@gmail.fi");
+var Kalle = new henkilo("Kalle", "Mutanen", "Koulutie 5", "08531", "Hikiö", "0427836485", "kalle.mutanen@luukku.fi");
+var Jussi = new henkilo("jussi", "Kauppi", "louhi 3", "20713", "Perä", "0462798559", "jussi.kauppi@hotmail.fi");
+
+
+console.log(Hannu);
+console.log(Katri);
+console.log(Mira);
+console.log(Kalle);
+console.log(Jussi);
---
+++ omia kokeiluja/IF/if.js
@@ -0,0 +1,29 @@
+//var ika = 15;
+
+//if (ika >= 15) {
+//  console.log("Tervetuloa");
+//} else{
+//  console.log("Olet liian nuori")
+//}
+timeLeft = prompt("Kuinka monta päivää on kesälomaan?");
+var timeLeft = "";
+ if (timeLeft <= 20) {
+   console.log("Kohta pääset kesä lomalle");
+ } else if (timeLeft <= 50) {
+    console.log("Jaksaa jaksaa");
+ } else if (timeLeft <= 100) {
+    console.log("Loppukiri häämöttää!");
+ } else {
+    console.log("Ei Kommentoitavaa");
+ }
+
+
+// var salis = "12kolme";
+ //if (salis == "12kolme") {
+  // console.log("Tervetuloa sisään!");
+// } else {
+  // console.log("Yritä Uudelleen!");
+ //}
+
+ //console.log("12kolme" == "12kolme");  // Boolean logiikka
+// console.log("password" == "12kolme");
